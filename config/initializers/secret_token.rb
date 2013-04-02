b1d9b008e1e0ccf08909414db5d73ead59a20205
Yenchi Lu <ylu42@academic.rrc.ca>
2013-04-02 13:44:28 -0500

    All files for multi-laptop cooperatoin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jasbooks::Application.config.secret_token = '30095fead37a501cf8ff17781b6699a0b4a4a935311726305a2911e20278dfe4a67cc774ade20780579ce7e08aa82a766a6f617a43fc53e86b669048e25280dc'
