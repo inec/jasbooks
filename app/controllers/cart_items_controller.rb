f32ee6a392dd5a76f309511fc15a3a666ceea665
Yenchi Lu <ylu42@academic.rrc.ca>
2013-04-06 15:11:16 -0500

    cart_items test

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,37 @@
 class CartItemsController < InheritedResources::Base
+	def create
+	@cart = current_cart
+	#book = Book.find((params[:book_id])
+	@cart_item = @cart.cart_items.build(:book_id => params[:book_id])
+  respond_to do |format|
+		if @cart_item.save
+		format.html { redirect_to(@cart_item.cart,:notice => 'Line item was successfully created.') }
+
+			else
+			format.html { render :action => "new" }
+
+			end
+		end
+	end
+
+
+
+	def index
+    @cart_items = CartItem.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+     
+    	end
+  	end
+
+
+   def show
+    @cart_item = CartItem.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      
+    end
+  end
 end
